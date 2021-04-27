f9c4db42732e23a66f83934ca3b1daf415cf0a25
LouFagan <lfagan@pixelmein.ca>
2021-04-27 09:36:57 -0400

    Project done. Last commit.

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,24 @@
-console.log("hello");
+const hamburger = document.querySelector('.menuMobile__close');
+const x = document.querySelector('.menuMobile__open');
+const mobileLinks = document.querySelector('.mobile-links');
+
+hamburger.addEventListener('click', () => {
+    mobileLinks.classList.toggle('hidden');
+    hamburger.classList.toggle('hidden');
+    x.classList.toggle('hidden');
+});
+x.addEventListener('click', () => {
+    mobileLinks.classList.toggle('hidden');
+    hamburger.classList.toggle('hidden');
+    x.classList.toggle('hidden');
+});
+
+
+
+
+/* function activeFunction(e, ev) {
+    for (var i = 0; i < e.children.length; i++) {
+        e.children[i].childNodes[0].className = "";
+    }
+    ev.target.className = "current-link";
+} */
